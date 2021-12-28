6623a350a519b696a23dacce079605eecd0f532a
YOPI 1337 <mrx.ga10@gmail.com>
2021-12-28 18:55:58 +0100

    ft_img done

--- ft_img.c
+++ ft_img.c
@@ -6,7 +6,7 @@
 /*   By: zyacoubi <zyacoubi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 17:47:42 by zyacoubi          #+#    #+#             */
-/*   Updated: 2021/12/23 19:02:15 by zyacoubi         ###   ########.fr       */
+/*   Updated: 2021/12/28 18:55:36 by zyacoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void    ft_img(t_so_long *game)
 {
-    game->t_path = "./assets/Path";
+    game->t_path = "./assets/Path.xpm";
     game->path = mlx_xpm_file_to_image(game->mlx,game->t_path,&game->i,&game->j);
-    game->t_ply = "./assets/player";
+    game->t_ply = "./assets/player.xpm";
     game->player = mlx_xpm_file_to_image(game->mlx,game->t_ply,&game->i,&game->j);
-    game->t_exit = "./assets/";
+    game->t_exit = "./assets/exit.xpm";
     game->exitus = mlx_xpm_file_to_image(game->mlx,game->t_exit,&game->i,&game->j);
-    game->t_col = "./assets/";
+    game->t_col = "./assets/collect.xpm";
     game->collect = mlx_xpm_file_to_image(game->mlx,game->t_col,&game->i,&game->j);
-    game->t_wall = "./assets/";
+    game->t_wall = "./assets/wall.xpm";
     game->wall = mlx_xpm_file_to_image(game->mlx,game->t_ply,&game->i,&game->j);
     
 }
