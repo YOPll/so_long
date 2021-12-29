e33023c9b36f3afaa9cd744694d12b8122e6cef7
YOPI 1337 <mrx.ga10@gmail.com>
2021-12-29 22:29:32 +0100

    ft_move to fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zyacoubi <zyacoubi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:12:21 by zyacoubi          #+#    #+#             */
-/*   Updated: 2021/12/29 21:54:30 by zyacoubi         ###   ########.fr       */
+/*   Updated: 2021/12/29 22:19:57 by zyacoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int main(int ac, char *av[])
     ft_img(&game);
     ft_map(&game);
     ft_mapt(&game);
+    mlx_hook(game.window, 2, (1L << 0), ft_move, &game);
+    mlx_hook(game.window, 17, (1L << 17), ft_exit, &game);
     mlx_loop(game.mlx);
-    //mlx_hook(game.mlx, 2, (1L << 0), ft_move, &game);
-    //mlx_hook(game.mlx, 17, (1L << 17), ft_exit, &game);
     return (0);
 }
