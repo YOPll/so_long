06057123ed0a2083e69580068449fd51966439ca
YOPI 1337 <mrx.ga10@gmail.com>
2021-12-30 18:19:20 +0100

    mandatory part done

--- ft_syncmap.c
+++ ft_syncmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_syncmap.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yopi <yopi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: zyacoubi <zyacoubi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/28 15:18:09 by zyacoubi          #+#    #+#             */
-/*   Updated: 2021/12/28 22:22:37 by yopi             ###   ########.fr       */
+/*   Updated: 2021/12/30 18:15:42 by zyacoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,17 @@ void    ft_syncmap(int x, int y, t_so_long *game)
     
     i = game->myplayer.v;
     j = game->myplayer.h;
-    if (game->map.map[i][j] == 'E' || game->map.map[i][j] == 'G')
+    if (game->map.map[x][y] == 'E' || game->map.map[x][y] == 'G')
     {
         ft_map_pla(i, j, game);
-        ft_mapexit(x, y, game);
         ft_mapwall(x, y, game);
         ft_map_path(x, y, game);
+        ft_mapexit(x, y, game);
     }
     else
     {
         ft_map_path(x, y, game);
-        if (game->map.map[i][j] == 'E')
+        if (game->map.map[x][y] == 'E')
         {
             ft_map_path(x, y, game);
             ft_mapexit(x, y, game);
